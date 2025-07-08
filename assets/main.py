16097b923057f2297b516a006f2c6147a749baba
zedMar65 <marius.jedinkus@gmail.com>
2025-07-08 13:29:43 +0300

    prod changes

--- assets/main.py
+++ assets/main.py
@@ -40,16 +40,16 @@ def init():
     watch_calendar()
 
 def main():
-    EmailSender.send_email(
-        to_email="test-vir3ld4k2@srv1.mail-tester.com",
-        subject=Flags.EMAIL_SUBJECT,
-        body=Flags.EMAIL_BODY,
-        is_html=False
-    )
-    # log("starting main script")
-    # monthly_thread = threading.Thread(target=check_and_run_monthly_task, daemon=True)
-    # monthly_thread.start()
-    # start_server()
+#    EmailSender.send_email(
+#        to_email="test-vir3ld4k2@srv1.mail-tester.com",
+#        subject=Flags.EMAIL_SUBJECT,
+#        body=Flags.EMAIL_BODY,
+#        is_html=False
+#    )
+    log("starting main script")
+    monthly_thread = threading.Thread(target=check_and_run_monthly_task, daemon=True)
+    monthly_thread.start()
+    start_server()
     return
 
 if __name__ == "__main__":
